f27176e5b26db53fec362bbf407bb3dba18e589a
Petko Todorov <p.todorov1015@gmail.com>
2025-11-18 13:23:35 +0200

    Introduces `BASE_URL` from environment and updates API exports.

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,1 +1,5 @@
-export const api = "https://corsproxy.io/?https://genius.com/api/search/multi?q=";
+const api = "https://corsproxy.io/?https://genius.com/api/search/multi?q=";
+
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+export { api, BASE_URL };
