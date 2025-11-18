0ff04af031e1141667a38d157d4c3f13e20f55ca
Petko Todorov <p.todorov1015@gmail.com>
2025-11-18 13:20:55 +0200

    Add CORS_ALLOWED_ORIGINS setting and ensure 'rest_framework' and 'youtube' are in INSTALLED_APPS

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'corsheaders',
+    'rest_framework',
+
+    'youtube',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +124,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
